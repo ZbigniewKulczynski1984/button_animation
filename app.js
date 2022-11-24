41a3d01ff7f50982410799146d016abf5971d03b
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-24 07:41:00 +0100

    code added

---
+++ app.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('.ripple');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const x = e.clientX
+        const y = e.clientY
+
+        // console.log(x, y);
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        // console.log(buttonTop, buttonLeft);
+    })
+})
